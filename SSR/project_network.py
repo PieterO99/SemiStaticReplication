e2d613ca72fd2f01d55924363cef2d7f4bf838dc
RiccardoAlberti <138592180+RiccardoAlberti@users.noreply.github.com>
2023-11-15 15:17:27 +0100

    Add files via upload

--- SSR/project_network.py
+++ SSR/project_network.py
@@ -1,7 +1,6 @@
 from tensorflow import keras
 from keras import layers
 from keras.callbacks import EarlyStopping
-import numpy as np
 
 
 # Define the network class
@@ -31,23 +30,21 @@ class SemiStaticNet(keras.models.Sequential):
             for layer, custom_weights in zip(self.layers, custom_weights_pairs):
                 layer.set_weights(custom_weights)
         else:
-            for layer in self.layers:
-                layer.kernel_initializer = keras.initializers.RandomUniform(minval=0.0, maxval=1.0)
+            self.layers[0].kernel_initializer = keras.initializers.RandomUniform(minval=0.0, maxval=1.0)
+            self.layers[1].kernel_initializer = keras.initializers.random_normal
 
 
 # Build the backward recursion that fits the network for every interval between monitoring dates
 def fitting(stock, option, weights=None, optimizer=keras.optimizers.Adam(learning_rate=0.001)):
-    rlnn = SemiStaticNet(weights, optimizer)
 
-    # normalize features:
-    normalized_stock = stock / np.max(stock)
+    rlnn = SemiStaticNet(weights, optimizer)
 
     # Define the EarlyStopping callback
     early_stopping = EarlyStopping(monitor='val_loss', patience=6, verbose=1, restore_best_weights=True)
 
     # Split the data into a training set and a validation set
     train_size = int(0.7 * len(stock))
-    x_train, x_val = normalized_stock[:train_size], normalized_stock[train_size:]
+    x_train, x_val = stock[:train_size], stock[train_size:]
     y_train, y_val = option[:train_size], option[train_size:]
 
     rlnn.fit(x_train, y_train, epochs=3000, batch_size=int(len(stock) / 10), verbose=0,
@@ -56,3 +53,15 @@ def fitting(stock, option, weights=None, optimizer=keras.optimizers.Adam(learnin
     final_weights = rlnn.get_weights()
 
     return final_weights
+
+
+def pre_training(stock, option, optimizer, epochs, split, weights=None):
+
+    rlnn = SemiStaticNet(weights, optimizer)
+
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, restore_best_weights=True)
+
+    rlnn.fit(stock, option, epochs=epochs, batch_size=int(len(stock) / 10), verbose=0,
+             validation_split=split, callbacks=[early_stopping])
+
+    return rlnn.get_weights()
